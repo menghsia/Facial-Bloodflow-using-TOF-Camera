b9e5ea7eb45b35c4e548f59770e42afd2614d485
grabad <128432541+grabad@users.noreply.github.com>
2023-04-03 20:19:03 -0400

    Add files via upload

---
+++ auto_matlab/process_thermal_SINGLE.m
@@ -0,0 +1,25 @@
+clear
+close all
+
+%% Load and process data
+[tb, bc, HRsig] = processRawData("../skvs/mat/auto_bfsig.mat", 'Before');
+
+%% Plot smoothed blood concentration
+figure()
+hold on
+plot(tb,smooth(bc(2,:),50));
+plot(tb,smooth(bc(1,:),50));
+plot(tb, smooth((bc(3,:)+bc(4,:))/2,50));
+
+xlabel('Time (s)')
+legend('Nose','Forehead','Cheek Average')
+ylabel('Relative Blood Concentration Change (a.u.)')
+%% Get HR Data
+[t_HR, HR] = getHR(HRsig);
+
+%% Plot HR Data
+figure()
+plot(t_HR, HR)
+title('Heartrate')
+xlabel('Time (seconds)')
+ylabel('Heart Rate (bpm)')
