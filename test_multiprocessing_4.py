ce7372dd1a246c0e9a4322fab08e018a714064a8
Muhsinun Chowdhury <11369216+MuhsinunC@users.noreply.github.com>
2023-05-07 00:15:40 -0400

    this code seems to let me share a memory buffer with a pool, but I still have to make this work with the memory buffer I created previously

---
+++ test_multiprocessing_4.py
@@ -0,0 +1,20 @@
+import multiprocessing as mp
+import ctypes
+
+def worker_function(i, shared_arr):
+    # Modify the shared array in-place
+    shared_arr[i] = i * 2
+
+if __name__ == '__main__':
+    # Create a Manager object and a shared array with 10 elements of type double
+    manager = mp.Manager()
+    arr_size = 10
+    shared_arr = manager.list([0.0] * arr_size)
+
+    # Create a pool of 16 worker processes
+    with mp.Pool(processes=16) as pool:
+        # Call worker_function() for each index in the shared array
+        pool.starmap(worker_function, [(i, shared_arr) for i in range(arr_size)])
+
+    # Print the contents of the shared array
+    print(list(shared_arr))
