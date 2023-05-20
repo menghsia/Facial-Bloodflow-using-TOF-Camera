9e2d8d7f4e70e916fbda3f04cdc2f8cbde633299
Muhsinun Chowdhury <11369216+MuhsinunC@users.noreply.github.com>
2023-05-19 23:17:10 -0400

    format document

--- test_new_facemesh.py
+++ test_new_facemesh.py
@@ -12,6 +12,7 @@ from mediapipe.tasks.python import vision
 urllib.request.urlretrieve("https://storage.googleapis.com/mediapipe-models/face_landmarker/face_landmarker/float16/1/face_landmarker.task",
                            "face_landmarker_v2_with_blendshapes.task")
 
+
 def draw_landmarks_on_image(rgb_image, detection_result):
     face_landmarks_list = detection_result.face_landmarks
     annotated_image = np.copy(rgb_image)
@@ -21,31 +22,31 @@ def draw_landmarks_on_image(rgb_image, detection_result):
         face_landmarks = face_landmarks_list[idx]
 
         # Draw the face landmarks.
-        face_landmarks_proto = landmark_pb2.NormalizedLandmarkList() # type: ignore
+        face_landmarks_proto = landmark_pb2.NormalizedLandmarkList()  # type: ignore
         face_landmarks_proto.landmark.extend([
             landmark_pb2.NormalizedLandmark(x=landmark.x, y=landmark.y, z=landmark.z) for landmark in face_landmarks # type: ignore
         ])
 
-        solutions.drawing_utils.draw_landmarks( # type: ignore
+        solutions.drawing_utils.draw_landmarks(  # type: ignore
             image=annotated_image,
             landmark_list=face_landmarks_proto,
-            connections=mp.solutions.face_mesh.FACEMESH_TESSELATION, # type: ignore
+            connections=mp.solutions.face_mesh.FACEMESH_TESSELATION,  # type: ignore
             landmark_drawing_spec=None,
-            connection_drawing_spec=mp.solutions.drawing_styles # type: ignore
+            connection_drawing_spec=mp.solutions.drawing_styles  # type: ignore
             .get_default_face_mesh_tesselation_style())
-        solutions.drawing_utils.draw_landmarks( # type: ignore
+        solutions.drawing_utils.draw_landmarks(  # type: ignore
             image=annotated_image,
             landmark_list=face_landmarks_proto,
-            connections=mp.solutions.face_mesh.FACEMESH_CONTOURS, # type: ignore
+            connections=mp.solutions.face_mesh.FACEMESH_CONTOURS,  # type: ignore
             landmark_drawing_spec=None,
-            connection_drawing_spec=mp.solutions.drawing_styles # type: ignore
+            connection_drawing_spec=mp.solutions.drawing_styles  # type: ignore
             .get_default_face_mesh_contours_style())
-        solutions.drawing_utils.draw_landmarks( # type: ignore
+        solutions.drawing_utils.draw_landmarks(  # type: ignore
             image=annotated_image,
             landmark_list=face_landmarks_proto,
-            connections=mp.solutions.face_mesh.FACEMESH_IRISES, # type: ignore
+            connections=mp.solutions.face_mesh.FACEMESH_IRISES,  # type: ignore
             landmark_drawing_spec=None,
-            connection_drawing_spec=mp.solutions.drawing_styles # type: ignore
+            connection_drawing_spec=mp.solutions.drawing_styles  # type: ignore
             .get_default_face_mesh_iris_connections_style())
 
     return annotated_image
