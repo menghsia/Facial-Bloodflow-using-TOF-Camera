b7cb8395bdb2d80cbef228fafde8abc315e51fc9
Muhsinun Chowdhury <11369216+MuhsinunC@users.noreply.github.com>
2023-06-01 16:25:47 -0400

    add type hints, update function description, fix input type issue with ImageDraw.Draw.polygon()

--- face_mesh_detector.py
+++ face_mesh_detector.py
@@ -278,10 +278,21 @@ class PhaseTwo():
 
         return bounding_box_pixels
     
-    def _get_pixels_within_ROI_bounding_box(self, bounding_box_pixels):
+    def _get_pixels_within_ROI_bounding_box(self, bounding_box_pixels: np.ndarray) -> np.ndarray:
         """
         Takes in the pixel coordinates that represent the bounding box for an ROI and returns
         the pixel coordinates of all pixels within the bounding box.
+
+        Args:
+            bounding_box_pixels (np.ndarray): An array of shape (n, 2), where n is the number of landmarks
+                that form the bounding box for the requested ROI. Each row represents the (x, y)
+                coordinates of a landmark pixel.
+
+        Returns:
+            np.ndarray: An array of shape (image_height, image_width) representing the pixels within
+                the bounding box. The pixels within the bounding box will have a value of 1, while
+                the remaining pixels will have a value of 0. The output array has the same dimensions
+                as the frame image.
         """
 
         # Create a black (0) grayscale image with the same dimensions as the frame
@@ -292,7 +303,7 @@ class PhaseTwo():
         # Draw a polygon on the mask_canvas using the ROI bounding box pixel coordinates
         # The polygon will be filled in with pixels with value 1, and the outline will
         # be 1 pixel wide with a value of 1 as well.
-        ImageDraw.Draw(mask_canvas).polygon(bounding_box_pixels, fill=1, outline=1, width=1)
+        ImageDraw.Draw(mask_canvas).polygon(bounding_box_pixels.tolist(), fill=1, outline=1, width=1)
 
         # Convert the mask_canvas image, with the filled-in polygon on it, to a numpy array
         # The array will have a shape of (self.image_height, self.image_width)
