7832c165cb4af19bf999da17a94b4ad2db19de5c
Muhsinun Chowdhury <11369216+MuhsinunC@users.noreply.github.com>
2023-06-02 20:03:06 -0400

    add TODO

--- face_mesh_module.py
+++ face_mesh_module.py
@@ -100,6 +100,8 @@ class FaceMeshDetector():
                     # Store pixel coordinates in array
                     landmarks_pixels[id] = (x, y)
 
+                    # TODO: Check if 468 landmarks are always detected in every possible input image
+
         return face_detected, landmarks_pixels
 
 
