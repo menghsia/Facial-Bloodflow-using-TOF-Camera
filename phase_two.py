c1592ead3f9e55a3dee6e49fb4a76a3685ad64a6
Syeda Reza <syerez@umich.edu>
2023-08-07 14:42:22 -0400

    commented out attempts to alter cheeknnose ROI

--- phase_two.py
+++ phase_two.py
@@ -433,6 +433,16 @@ class PhaseTwo():
         except KeyError:
             raise KeyError(f"ERROR: The provided roi_name \"{roi_name}\" does not match any of the predefined ROIs.")
 
+        # if roi_name == 'cheek_n_nose':
+        #     bounding_box_pixels[0][1] = bounding_box_pixels[0][1] + 2
+        #     bounding_box_pixels[1][1] = bounding_box_pixels[1][1] + 2
+        #     bounding_box_pixels[2][1] = bounding_box_pixels[2][1] - 0
+        #     bounding_box_pixels[3][1] = bounding_box_pixels[3][1] - 0
+        #     bounding_box_pixels[0][0] = bounding_box_pixels[0][0] + 5
+        #     bounding_box_pixels[1][0] = bounding_box_pixels[1][0] - 2
+        #     bounding_box_pixels[2][0] = bounding_box_pixels[2][0] - 2
+        #     bounding_box_pixels[3][0] = bounding_box_pixels[3][0] + 5
+
         return bounding_box_pixels
     
     def _get_pixels_within_ROI_bounding_box(self, bounding_box_pixels: np.ndarray) -> np.ndarray:
