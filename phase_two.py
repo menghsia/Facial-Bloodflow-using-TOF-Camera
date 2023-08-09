589e3588986d396f5b720a8df7929adcedbb5b73
DESKTOP-IUSQBA4\MNI Lab <98334650+dobreal@users.noreply.github.com>
2023-08-09 16:41:44 -0400

    commented out ROI adjustments

--- phase_two.py
+++ phase_two.py
@@ -434,10 +434,10 @@ class PhaseTwo():
             raise KeyError(f"ERROR: The provided roi_name \"{roi_name}\" does not match any of the predefined ROIs.")
 
         if roi_name == 'cheek_n_nose':
-            bounding_box_pixels[2][1] = bounding_box_pixels[2][1] - 2
-            bounding_box_pixels[3][1] = bounding_box_pixels[3][1] - 2
-            bounding_box_pixels[1][1] = bounding_box_pixels[1][1] + 2
-            bounding_box_pixels[0][1] = bounding_box_pixels[0][1] + 2
+            # bounding_box_pixels[2][1] = bounding_box_pixels[2][1] - 2
+            # bounding_box_pixels[3][1] = bounding_box_pixels[3][1] - 2
+            # bounding_box_pixels[1][1] = bounding_box_pixels[1][1] + 2
+            # bounding_box_pixels[0][1] = bounding_box_pixels[0][1] + 2
             # bounding_box_pixels[0][0] = bounding_box_pixels[0][0] + 3
             # bounding_box_pixels[1][0] = bounding_box_pixels[1][0] - 3
             # bounding_box_pixels[2][0] = bounding_box_pixels[2][0] - 3
